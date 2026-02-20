4c30b93bf35dda783c741e42f789a30f90851625
SylabTv <sylab.studio@gmail.com>
2026-02-20 10:05:34 +0000

    test git ignore .env

---
+++ jour7/correction-mini-projet/connexion.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Utilisation des variables d'environnement
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$user = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
+// echo "<pre>";
+// var_dump($db);
+// echo "</pre>";
